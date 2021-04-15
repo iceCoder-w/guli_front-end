6c0058a6065873bd81dfa5d0bb14f7cfa9482ae7
王冰炜 <619445114@qq.com>
2021-04-15 10:21:46 +0800

    chore: build the basic structure, initialize the teacher management views, add 'add', 'delete' and 'query' functions, initialize the global request method and teacher operate api

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+
+export default {
+  // 讲师列表（条件查询分页）
+  getTeacherListPage(current, limit, teacherQuery) {
+    return request({
+      url: `/eduservice/edu-teacher/pageTeacherCondition/${current}/${limit}`,
+      method: 'post',
+      data: teacherQuery
+    })
+  },
+  removeById(id) {
+    return request({
+      url: `/eduservice/edu-teacher/deleteByID/${id}`,
+      method: 'delete'
+    })
+  },
+  save(teacher) {
+    return request({
+      url: `/eduservice/edu-teacher/addTeacher/`,
+      method: 'post',
+      data: teacher
+    })
+  }
+
+}
