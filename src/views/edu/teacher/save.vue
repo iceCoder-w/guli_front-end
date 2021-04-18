134f4dc33fc4b7823e31bfcbe8fef9a0c459a6ed
王冰炜 <619445114@qq.com>
2021-04-18 10:35:29 +0800

    feat: realize the function of uploading avatar for teacher module

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -29,7 +29,32 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
       </el-form-item>
 
-      <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+
+        <!-- 头像缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+    v-show：是否显示上传组件
+    :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+    :url：后台上传的url地址
+    @close：关闭上传组件
+    @crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/eduoss/fileoss'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+
+      </el-form-item>
 
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
@@ -40,7 +65,11 @@
 </template>
 <script>
 import teacherApi from '@/api/edu/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
+
 export default {
+  components: { ImageCropper, PanThumb },
   data() {
     return {
       teacher: {
@@ -51,6 +80,9 @@ export default {
         intro: '',
         avatar: ''
       },
+      imagecropperShow: false, // 上传头像弹框组件是否显示
+      imagecropperKey: 0, // 上传组件唯一标识key值
+      BASE_API: process.env.BASE_API, // 获取dev.env.js中请求地址
       saveBtnDisabled: false // 保存按钮是否禁用,
     }
   },
@@ -79,12 +111,14 @@ export default {
       }
     },
 
+    // 判断是修改还是添加
     saveOrUpdate() {
       this.saveBtnDisabled = true
-      // 判断是修改还是添加
       if (!this.teacher.id) {
+        // 新增
         this.saveData()
       } else {
+        // 修改
         this.updateDate()
       }
     },
@@ -143,6 +177,20 @@ export default {
     // 清空
     resetData() {
       this.teacher = {}
+    },
+
+    // 关闭上传弹框
+    close() {
+      this.imagecropperShow = false
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
+
+    // 上传成功
+    cropSuccess(data) {
+      this.imagecropperShow = false
+      // 上传成功后返回头像url地址
+      this.teacher.avatar = data.url
+      this.imagecropperKey = this.imagecropperKey + 1
     }
   }
 }
