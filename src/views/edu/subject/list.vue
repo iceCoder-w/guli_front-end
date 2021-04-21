05774b1a78561ae5903b6ce8079a41e8008fceef
王冰炜 <619445114@qq.com>
2021-04-21 19:58:19 +0800

    feat: complete the view of receiving back-end tree format information, and displaying in tree format list

--- src/views/edu/subject/list.vue
+++ src/views/edu/subject/list.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    课程分类列表
+  <div class="app-container">
+    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
+
+    <el-tree
+      ref="tree2"
+      :data="data2"
+      :props="defaultProps"
+      :filter-node-method="filterNode"
+      class="filter-tree"
+      default-expand-all
+    />
+
   </div>
 </template>
 
 <script>
+import subject from '@/api/edu/subject'
 export default {
-  name: 'List'
+  data() {
+    return {
+      filterText: '',
+      data2: [], // 返回所有的课程分类数据
+      defaultProps: {
+        children: 'children',
+        label: 'title'
+      }
+    }
+  },
+
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val)
+    }
+  },
+  created() {
+    this.getAllSubjectList()
+  },
+
+  methods: {
+    getAllSubjectList() {
+      subject.getSubjectList()
+        .then(response => {
+          this.data2 = response.data.list
+        })
+    },
+
+    filterNode(value, data) {
+      if (!value) return true
+      return data.title.indexOf(value) !== -1
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
