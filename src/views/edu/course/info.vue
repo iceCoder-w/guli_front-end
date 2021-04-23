96e96e9362568d85df7940421efc166e1cbbb4fa
王冰炜 <619445114@qq.com>
2021-04-23 17:31:29 +0800

    feat: add course cover upload

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -15,7 +15,7 @@
         <el-input v-model="courseInfo.title" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
       </el-form-item>
 
-      <!-- 所属分类 TODO -->
+      <!-- 所属分类 -->
       <el-form-item label="课程分类">
         <!-- 一级分类 -->
         <el-select
@@ -57,12 +57,22 @@
         <el-input-number :min="0" v-model="courseInfo.lessonNum" controls-position="right" placeholder="请填写课程的总课时数"/>
       </el-form-item>
 
-      <!-- 课程简介 TODO -->
+      <!-- 课程简介 -->
       <el-form-item label="课程简介">
         <el-input v-model="courseInfo.description" placeholder=" 示例：机器学习是一门多领域交叉学科，专门研究计算机怎样模拟或实现人类的学习行为。"/>
       </el-form-item>
 
-      <!-- 课程封面 TODO -->
+      <!-- 课程封面 -->
+      <el-form-item label="课程封面">
+        <el-upload
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :action="BASE_API+'/eduoss/fileoss'"
+          class="avatar-uploader">
+          <img :src="courseInfo.cover">
+        </el-upload>
+      </el-form-item>
 
       <el-form-item label="课程价格">
         <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元"/> 元
@@ -90,12 +100,13 @@ export default {
         teacherId: '',
         lessonNum: 0,
         description: '',
-        cover: '',
+        cover: '/static/01.png',
         price: 0
       }, // 课程基本信息对象，用于数据封装
       teacherList: [], // 封装所有讲师的数据
       subjectOneList: [], // 一级分类
-      subjectTwoList: [] // 二级分类
+      subjectTwoList: [], // 二级分类
+      BASE_API: process.env.BASE_API // 接口API地址
     }
   },
 
@@ -143,6 +154,24 @@ export default {
           this.courseInfo.subjectId = '' // 每次选完一级，清空一下二级（多次选择一级时，会有上次遗留的二级内容，不美观）
         }
       }
+    },
+
+    // 课程封面上传成功调用的方法
+    handleAvatarSuccess(res) {
+      this.courseInfo.cover = res.data.url
+    },
+    // 上传之前，添加一些限制条件
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/png'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 PNG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
     }
   }
 }
