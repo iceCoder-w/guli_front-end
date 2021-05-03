a515fc3334d724620bf4be9ae55e51ece65d6f95
王冰炜 <619445114@qq.com>
2021-05-04 03:15:02 +0800

    feat: realize the upload video function of adding section

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -79,7 +79,25 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="上传视频">
-          <!-- TODO -->
+          <el-upload
+            :on-success="handleVodUploadSuccess"
+            :on-remove="handleVodRemove"
+            :before-remove="beforeVodRemove"
+            :on-exceed="handleUploadExceed"
+            :file-list="fileList"
+            :action="BASE_API+'/eduvod/video/uploadAliyunVideo'"
+            :limit="1"
+            class="upload-demo">
+            <el-button size="small" type="primary">上传视频</el-button>
+            <el-tooltip placement="right-end">
+              <div slot="content">最大支持1G，<br>
+                支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+                GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+                MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+                SWF、TS、VOB、WMV、WEBM 等视频格式上传</div>
+              <i class="el-icon-question"/>
+            </el-tooltip>
+          </el-upload>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -109,10 +127,13 @@ export default {
         title: '',
         sort: 0,
         isFree: 0,
-        videoSourceId: ''
+        videoSourceId: '',
+        videoOriginalName: '' // 视频名称
       }, // 封装小节数据
       dialogChapterFormVisible: false, // 添加和修改章节表单弹框是否显示
-      dialogVideoFormVisible: false // 添加和修改小节表单弹框是否显示
+      dialogVideoFormVisible: false, // 添加和修改小节表单弹框是否显示
+      fileList: [], // 上传文件列表
+      BASE_API: process.env.BASE_API // 接口API地址
     }
   },
 
@@ -328,6 +349,16 @@ export default {
         .then(response => {
           this.video = response.data.video
         })
+    },
+
+    // 上传视频成功调用的方法
+    handleVodUploadSuccess(response, file, fileList) {
+      this.video.videoSourceId = response.data.videoId
+      this.video.videoOriginalName = file.name
+    },
+    // 上传视频之前调用的方法
+    handleUploadExceed() {
+      this.$message.warning('想要重新上传视频，请先删除已上传的视频')
     }
   }
 }
